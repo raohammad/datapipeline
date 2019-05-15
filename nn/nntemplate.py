caa8bbcc1cea4161ef639da27585ba3492d62dcb
Hammad Aslam Khan <raohammad@gmail.com>
2019-05-15 10:22:42 +0200

    changed license

--- nn/nntemplate.py
+++ nn/nntemplate.py
@@ -1,3 +1,13 @@
+__author__ = "Hammad Aslam Khan"
+__description__ = "Data pipeline for Inferencing on ONNX Neural Networks"
+__copyright__ = "Copyright 2019"
+__credits__ = ["Hammad"]
+__license__ = "MIT"
+__version__ = "1.0.1"
+__maintainer__ = "Hammad"
+__email__ = "raohammad@gmail.com"
+__status__ = "NA"
+
 from nn.base import NNBase
 from common.args import Args
 
--- target/targettemplate.py
+++ target/targettemplate.py
@@ -1,7 +1,8 @@
 __author__ = "Hammad Aslam Khan"
-__copyright__ = "Copyright 2019, Data Pipeline for Inferencing on ONNX Neural Networks"
+__description__ = "Data pipeline for Inferencing on ONNX Neural Networks"
+__copyright__ = "Copyright 2019"
 __credits__ = ["Hammad"]
-__license__ = "GPL"
+__license__ = "MIT"
 __version__ = "1.0.1"
 __maintainer__ = "Hammad"
 __email__ = "raohammad@gmail.com"
